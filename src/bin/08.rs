4bb2f91dc5b9e3e47e883eb3951d95194559dd79
magnusrodseth <magnus.rodseth@gmail.com>
2023-12-08 10:39:47 +0100

    day8 part 2 tests pass

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 const START_LABEL: &str = "AAA";
 const END_LABEL: &str = "ZZZ";
@@ -12,11 +12,32 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug, Clone)]
+impl Direction {
+    fn parse_directions(input: &str) -> Vec<Self> {
+        input
+            .chars()
+            .map(|c| match c {
+                'L' => Direction::Left,
+                'R' => Direction::Right,
+                _ => panic!("invalid direction"),
+            })
+            .collect::<Vec<Direction>>()
+    }
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Node {
     label: Label,
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
+}
+
+fn all_labels_end_with_z(labels: &HashSet<String>, nodes: &HashMap<String, Node>) -> bool {
+    labels.iter().all(|label| {
+        nodes
+            .get(label)
+            .map_or(false, |node| node.label.ends_with("Z"))
+    })
 }
 
 fn create_graph(input: &str) -> HashMap<Label, Node> {
@@ -32,7 +53,7 @@ fn create_graph(input: &str) -> HashMap<Label, Node> {
         nodes_map.insert(label, (left_label.to_string(), right_label.to_string()));
     });
 
-    let mut nodes: HashMap<String, Node> = HashMap::new();
+    let mut nodes: HashMap<Label, Node> = HashMap::new();
     for (label, _) in nodes_map.iter() {
         nodes.insert(
             label.clone(),
@@ -59,17 +80,8 @@ fn create_graph(input: &str) -> HashMap<Label, Node> {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let directions = input
-        .lines()
-        .nth(0)
-        .expect("should have first line")
-        .chars()
-        .map(|c| match c {
-            'L' => Direction::Left,
-            'R' => Direction::Right,
-            _ => panic!("invalid direction"),
-        })
-        .collect::<Vec<Direction>>();
+    let directions =
+        Direction::parse_directions(input.lines().nth(0).expect("should have first line"));
 
     let nodes = create_graph(input);
 
@@ -102,7 +114,41 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let directions =
+        Direction::parse_directions(input.lines().nth(0).expect("should have first line"));
+
+    let nodes = create_graph(input);
+
+    let starting_labels = nodes
+        .iter()
+        .filter(|(_, node)| node.label.ends_with('A'))
+        .map(|(_, node)| node.label.clone())
+        .collect::<HashSet<String>>();
+
+    let mut visited_labels = starting_labels;
+    let mut steps: usize = 0;
+
+    while !all_labels_end_with_z(&visited_labels, &nodes) {
+        let mut next_visited_labels = HashSet::new();
+        let direction = &directions[steps % directions.len()];
+
+        for label in visited_labels.iter() {
+            let node = nodes.get(label).expect("Node must exist for label");
+            let next_label = match direction {
+                Direction::Left => node.left.as_ref().map(|n| n.label.clone()),
+                Direction::Right => node.right.as_ref().map(|n| n.label.clone()),
+            };
+
+            if let Some(label) = next_label {
+                next_visited_labels.insert(label);
+            }
+        }
+
+        visited_labels = next_visited_labels;
+        steps += 1;
+    }
+
+    Some(steps as u32)
 }
 
 advent_of_code::main!(8);
@@ -141,7 +187,18 @@ ZZZ = (ZZZ, ZZZ)",
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", 8));
-        assert_eq!(result, None);
+        let result = part_two(
+            "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)",
+        );
+        assert_eq!(result, Some(6));
     }
 }
